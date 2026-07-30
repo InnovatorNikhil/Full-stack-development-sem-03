5ab28def6b0daab453f31cc06d9967e6abe2ec3a
Innovator Nikhil <nikkdubey23@gmail.com>
2026-07-30 10:22:29 +0530

    file added

---
+++ Day1/app.js
@@ -0,0 +1,50 @@
+//function in javascript
+//synchronous javascript
+
+// console.log("basic java script function known an sychrobous ");
+
+// function hell0(){
+//     console.log("this is js function");
+
+
+// }
+
+// hello();
+// console.log("sycjronous javascript");
+
+//Asynchronous java script
+//arrow function 
+//variable: var,let and const
+//syntax:()=>{}
+    // const hello=()=>{
+    //   console.log("asynch function");
+    //   setTimeout(()=>{
+    //  console.log("A");
+    //   },2000)
+    // }
+
+
+    // hello();
+    // console.log("b");
+
+
+
+    //function as parameter
+    function hello(n1,n2){
+      console.log(n1+n2);
+      console.log(argunments);
+
+    }
+
+    let a=10;
+    let b=20;
+
+    hello(a,b);
+   const app= ()=>{
+    console.log(arguments);
+    console.log(window);
+    
+
+   }
+   app();
+
