e7595b00bafd0c00c1df093a966e04ad24f42484
Innovator Nikhil <nikkdubey23@gmail.com>
2026-08-29 01:43:13 +0530

    changes occur

---
+++ Day8.js
@@ -0,0 +1,14 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write("<h1>This is my own server</h1>");
+    res.write("Hello World");
+
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
