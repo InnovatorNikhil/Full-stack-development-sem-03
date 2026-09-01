ece3c17f3d64b6b46389a2e6a039df2e91c7d216
Innovator Nikhil <nikkdubey23@gmail.com>
2026-09-01 16:27:05 +0530

    rest api

---
+++ RestApi/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app=express();
+
+app.use(express.json());
+let users=[{
+    id:1,
+    name:"A",
+    email:"a@abes.ac.in"
+},
+{
+    id:2,
+    name:"B",
+    email:"b@abes.ac.in"
+}];
+
+app.get("/users",(req,res)=>{
+   res.json(users);
+});
+
+app.post("/users",(req,res)=>{
+    const user={
+        id:users.length+1,
+        name:req.body.name,
+        email:req.body.email
+    };
+
+    users.push(users);
+    res.json(user);
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+
